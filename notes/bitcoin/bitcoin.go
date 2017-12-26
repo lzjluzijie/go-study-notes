8f40d52362737c900ef8e7fcb534c9f6a237f536
Halulu <lzjluzijie@gmail.com>
2017-12-26 15:46:53 +0800

    Add generate bitcoin address

--- notes/bitcoin/bitcoin.go
+++ notes/bitcoin/bitcoin.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 
 	"github.com/decred/base58"
+	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/urfave/cli"
+	"golang.org/x/crypto/ripemd160"
 	"golang.org/x/crypto/sha3"
 )
 
@@ -14,15 +16,33 @@ func NewWallet(c *cli.Context) error {
 	s := []byte("Hello World!")
 	h1 := sha3.Sum256(s)
 	h2 := sha3.Sum256(h1[:])
-	fmt.Printf("Secp256k1 private key in hexadecimal is : %x\n", h2)
+	private := h2[:]
+	fmt.Printf("Secp256k1 private key in hexadecimal is : %X\n", h2)
 
-	//WIF
-	k := append([]byte{0x80}, h2[:]...)
-	s1 := sha256.Sum256(k)
-	s2 := sha256.Sum256(s1[:])
-	key := append(k, s2[:4]...)
-	b58 := base58.Encode(key)
-	fmt.Printf("WIF private key is : %s\n", b58)
+	//WIF private key
+	k := append([]byte{0x80}, private...)
+	hk := sha2(k)
+	key := append(k, hk[:4]...)
+	wif := base58.Encode(key)
+	fmt.Printf("WIF private key is : %s\n", wif)
+
+	//Address
+	_, pub := secp256k1.PrivKeyFromBytes(private)
+	s256 := sha256.Sum256(pub.SerializeUncompressed())
+	r := ripemd160.New()
+	r.Write(s256[:])
+	r160 := r.Sum(nil)
+	addr := append([]byte{0x00}, r160...)
+	ha := sha2(addr)
+	address := base58.Encode(append(addr, ha[:4]...))
+	fmt.Printf("Address is : %s\n", address)
 
 	return nil
+}
+
+///SHA-256 twice
+func sha2(data []byte) (hash []byte) {
+	h1 := sha256.Sum256(data)
+	h2 := sha256.Sum256(h1[:])
+	return h2[:]
 }
